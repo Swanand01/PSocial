42b9941527f6a112d0cac4816e91955f08a7c45b
chaitanya-Uike <chaitanyaguike@gmail.com>
2023-02-16 21:11:29 +0530

    register page bug fixed

--- Views/Register/index.jsx
+++ Views/Register/index.jsx
@@ -15,83 +15,81 @@ import API_ENDPOINTS from "../../API/endpoints";
 import useCustomInput from "../../Components/Input/useCustomInput";
 
 function RegisterScreen({ navigation }) {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [password2, setPassword2] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
-    const [sendRegisterRequest, registerData, registerLoading] = useFetch();
-    const [sendLoginRequest, loginData, loginLoading] = useFetch();
-    const [cookies, setCookie] = useCookies("access_token", { path: "/" });
-    const [Toast, showToast] = useToast();
-    
-    const [UsernameInput, playUsernameError] = useCustomInput();
-    const [EmailInput, playEmailError] = useCustomInput();
-    const [PasswordInput, playPasswordError] = useCustomInput();
-    const [Password2Input, playPassword2Error] = useCustomInput();
+  const [sendRegisterRequest, registerData, registerLoading] = useFetch();
+  const [sendLoginRequest, loginData, loginLoading] = useFetch();
+  const [cookies, setCookie] = useCookies("access_token", { path: "/" });
+  const [Toast, showToast] = useToast();
 
-    function register() {
-        if (username.trim() === "") {
-          playUsernameError();
-          return;
+  const [UsernameInput, playUsernameError] = useCustomInput();
+  const [EmailInput, playEmailError] = useCustomInput();
+  const [PasswordInput, playPasswordError] = useCustomInput();
+  const [Password2Input, playPassword2Error] = useCustomInput();
+
+  function register() {
+    if (username.trim() === "") {
+      playUsernameError();
+      return;
+    }
+    if (email.trim() === "") {
+      playEmailError();
+      return;
+    }
+    if (password.trim() === "") {
+      playPasswordError();
+      return;
+    }
+    if (password2.trim() === "") {
+      playPassword2Error();
+      return;
+    }
+
+    if (password.trim() !== password2.trim()) {
+      playPassword2Error();
+      playPasswordError();
+      showToast("passwords do not match", 500);
+      return;
+    }
+
+    sendRegisterRequest({
+      url: API_ENDPOINTS.BASE_URL + API_ENDPOINTS.REGISTER,
+      method: "POST",
+      body: {
+        user_name: username.trim(),
+        password: password.trim(),
+        email: email.trim(),
+        password2: password2.trim(),
+      },
+      headers: {},
+      callback: (data, status) => {
+        if (status === 201 && data.user_name && data.email) {
+          sendLoginRequest({
+            url: API_ENDPOINTS.BASE_URL + API_ENDPOINTS.LOGIN,
+            method: "POST",
+            body: { user_name: username.trim(), password: password.trim() },
+            headers: {},
+            callback: (data, status) => {
+              if (status === 200 && data.access) {
+                setCookie("access_token", data.access);
+                showToast("Registered successfully", 200, () => {
+                  navigation.navigate("Home");
+                });
+              } else if (status === 400) {
+                showToast("Something went wrong.", 500);
+                setBtnEnable(true);
               }
-        if (email.trim() === "") {
-          playEmailError();
-          return;
+            },
+            includeAccessToken: false,
+          });
         }
-        if (password.trim() === "") {
-          playPasswordError();
-          return;
-        }
-        if (password2.trim() === "") {
-          playPassword2Error();
-          return;
-        }
-
-        if (password.trim() !== password2.trim()) {
-          playPassword2Error();
-          playPasswordError();
-          showToast("passwords do not match", 500);
-          return;
-        }
-        
-        sendRegisterRequest(
-            {
-                url: API_ENDPOINTS.BASE_URL + API_ENDPOINTS.REGISTER,
-                method: "POST",
-                body: {
-                    user_name: username.trim(),
-                    password: password.trim(),
-                    email: email.trim(),
-                    password2: password2.trim()
-                },
-                headers: {},
-                callback: (data, status) => {
-                    if (status === 201 && data.user_name && data.email) {
-                        sendLoginRequest(
-                            {
-                                url: API_ENDPOINTS.BASE_URL + API_ENDPOINTS.LOGIN,
-                                method: "POST",
-                                body: { user_name: username.trim(), password: password.trim() },
-                                headers: {},
-                                callback: (data, status) => {
-                                    if (status === 200 && data.access) {
-                                        setCookie("access_token", data.access);
-                                        showToast("Registered successfully", 200, () => { navigation.navigate("Home") })
-                                    } else if (status === 400) {
-                                        showToast("Something went wrong.", 500)
-                                        setBtnEnable(true);
-                                    }
-                                },
-                                includeAccessToken: false
-                            }
-                        );
-                    }
-                },
-                includeAccessToken: false
-            }
-        );
-
+      },
+      includeAccessToken: false,
+    });
+  }
 
   return (
     <MainContainer>
@@ -179,6 +177,5 @@ const style = StyleSheet.create({
     marginTop: 20,
   },
 });
-
 
 export default RegisterScreen;
